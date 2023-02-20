14f0a92591e4c72f1df869ab3f99a66b33e5f74c
LeLwy <Louis.Chancioux@gmail.com>
2023-02-20 12:55:59 +0100

    Dynamisation des pages

--- wp-content/themes/yep/front-page.php
+++ wp-content/themes/yep/front-page.php
@@ -1,3 +1,10 @@
+<?php  
+
+$menuItems = getNavigationMenu();  
+$data = getHomepageData();  
+  
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -20,93 +27,102 @@
             </figure>
             <nav>
                 <ul>
-                    <li><a href="">accueil</a></li>
-                    <li><a href="">à propos</a></li>
-                    <li><a href="">mes projets</a></li>
-                    <li><a href="">me contacter</a></li>
+                    <?php foreach($menuItems as $item) { ?>  
+                        <li>  
+                            <a href="<?= $item->url ?>">  
+                                <?= $item->title ?>  
+                            </a>  
+                        </li>  
+                    <?php  
+                    }  
+                    ?>
                 </ul>
             </nav>
         </header>
-        <section>
-            <h1>yepfolio</h1>
-            <h2>développement web et web mobile</h2>
-        </section>
-        <section>
-            <h2>yepfolio en quelques mots</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatibus at temporibus modi. Dolor molestiae fugit rem, tempore, eos eaque quos incidunt cupiditate, molestias sunt asperiores labore recusandae magnam sapiente. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aliquam non recusandae libero molestias. Quibusdam voluptate quidem repellat quasi asperiores eligendi ad quaerat facere deleniti. Itaque quasi dolores natus enim.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo dignissimos aliquid placeat error sunt, minima nisi voluptatibus ducimus minus.</p>
-        </section>
-        <section>
-            <h2>mes derniers projets</h2>
-            <article>
-                <section>
-                    <figure>
-                        <img src="assets/images/pharmacy.jpeg" alt="">
-                        <figcaption></figcaption>
-                    </figure>
-                    <h4>application santé</h4>
-                    <h3>Pharmacie de Maurepas</h3>
-                </section>
-            </article>
-            <article>
-                <section>
-                    <figure>
-                        <img src="assets/images/librairie.jpeg" alt="">
-                        <figcaption></figcaption>
-                    </figure>
-                    <h4>site e-commerce</h4>
-                    <h3>Librairie l'écume des jours</h3>
-                </section>
-            </article>
-            <article>
-                <section>
-                    <figure>
-                        <img src="assets/images/boulangerie.jpeg" alt="">
-                        <figcaption></figcaption>
-                    </figure>
-                    <h4>site vitrine</h4>
-                    <h3>Boulangerie Galtier</h3>
-                </section>
-            </article>
-            <article>
-                <section>
-                    <figure>
-                        <img src="assets/images/mairie.jpeg" alt="">
-                        <figcaption></figcaption>
-                    </figure>
-                    <h4>site institutionnel</h4>
-                    <h3>Mairie de Ploutruc</h3>
-                </section>
-            </article>
-            <article>
-                <section>
-                    <figure>
-                        <img src="assets/images/festival.jpeg" alt="">
-                        <figcaption></figcaption>
-                    </figure>
-                    <h4>site promotionnel</h4>
-                    <h3>Festival des choses</h3>
-                </section>
-            </article>
-        </section>
-        <section>
-            <h2>me contacter</h2>
-            <form action="">
-                <fieldset>
-                    <label for="user-name">prénom + nom *</label>
-                    <input type="text" name="name" id="user-name">
-                </fieldset>
-                <fieldset>
-                    <label for="user-email">email *</label>
-                    <input type="email" name="email" id="user-email">
-                </fieldset>
-                <fieldset>
-                    <label for="user-message">message</label>
-                    <input type="textarea" name="message" id="user-message">
-                </fieldset>
-                <input type="submit" value="envoyer">
-            </form>
-        </section>
+        <main>
+
+            <section id="title-section">
+                <h1>yepfolio</h1>
+                <h2>développement web et web mobile</h2>
+            </section>
+            <section id="about-section">  
+                <h2><?= $data["a-propos"]["titre"] ?></h2>  
+                <p>  
+                    <?= $data["a-propos"]["contenu"] ?>  
+                </p>  
+            </section>
+            <section id="projects-section">
+                <h2>mes derniers projets</h2>
+                <article>
+                    <section>
+                        <figure>
+                            <img src="assets/images/pharmacy.jpeg" alt="">
+                            <figcaption></figcaption>
+                        </figure>
+                        <h4>application santé</h4>
+                        <h3>Pharmacie de Maurepas</h3>
+                    </section>
+                </article>
+                <article>
+                    <section>
+                        <figure>
+                            <img src="assets/images/librairie.jpeg" alt="">
+                            <figcaption></figcaption>
+                        </figure>
+                        <h4>site e-commerce</h4>
+                        <h3>Librairie l'écume des jours</h3>
+                    </section>
+                </article>
+                <article>
+                    <section>
+                        <figure>
+                            <img src="assets/images/boulangerie.jpeg" alt="">
+                            <figcaption></figcaption>
+                        </figure>
+                        <h4>site vitrine</h4>
+                        <h3>Boulangerie Galtier</h3>
+                    </section>
+                </article>
+                <article>
+                    <section>
+                        <figure>
+                            <img src="assets/images/mairie.jpeg" alt="">
+                            <figcaption></figcaption>
+                        </figure>
+                        <h4>site institutionnel</h4>
+                        <h3>Mairie de Ploutruc</h3>
+                    </section>
+                </article>
+                <article>
+                    <section>
+                        <figure>
+                            <img src="assets/images/festival.jpeg" alt="">
+                            <figcaption></figcaption>
+                        </figure>
+                        <h4>site promotionnel</h4>
+                        <h3>Festival des choses</h3>
+                    </section>
+                </article>
+            </section>
+            <section id="contact-section">
+                <h2>me contacter</h2>
+                <form action="">
+                    <fieldset>
+                        <label for="user-name">prénom + nom *</label>
+                        <input type="text" name="name" id="user-name">
+                    </fieldset>
+                    <fieldset>
+                        <label for="user-email">email *</label>
+                        <input type="email" name="email" id="user-email">
+                    </fieldset>
+                    <fieldset>
+                        <label for="user-message">message</label>
+                        <input type="textarea" name="message" id="user-message">
+                    </fieldset>
+                    <input type="submit" value="envoyer">
+                </form>
+            </section>
+        </main>
         <?php wp_footer(); ?>
     </body>
 </html>
